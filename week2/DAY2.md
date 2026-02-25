4e265a46de231bbac619b7bc6c1b1e8f022426f6
Tony405-spec <kengatony@gmail.com>
2026-02-25 14:59:30 +0300

    Week 2 Day 2: Added DAY2 documentation and test_pull.py

---
+++ week2/DAY2.md
@@ -0,0 +1,7 @@
+﻿# Day 2: Pulling from Docker Hub
+Tested Pulling image on fresh environment
+Image pulled successfully
+Tensorflow works
+CIFAR-10 accessible
+Anyone can now reproduce my environment with:
+docker pull tonykenga405/cifar10-env:tf2.13
---
+++ week2/test_pull.py
@@ -0,0 +1,5 @@
+﻿import tensorflow as tf
+print('Testing CIFAR-10 access in pulled image...')
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+print(f'Success! Training data shape: {x_train.shape}')
+print('Image works perfectly!')
